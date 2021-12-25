20d2d6837d52af79b12aed1e36b5d5cc566142a5
omarkamoun06 <omar.kamoun@istic.u-carthage.tn>
2021-12-25 10:29:39 +0100

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-import express from 'express';
+import express from 'express' ;
 import { engine } from 'express-handlebars';
 const app = express()
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://localhost/projet_node', { useNewUrlParser: true });
+
+const Review = mongoose.model('Review', {
+  title: String,
+  movieTitle: String
+});
 
 app.engine('handlebars',engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-app.get('/', (req, res) => {
+app.get('/views', (req, res) => {
   res.render('home', { msg : 'Handlebars are Cool!' });
+})
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  { title: "Great Review", movieTitle: "Batman II" },
+  { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// INDEX
+app.get('/', (req, res) => {
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 app.listen(3000);
